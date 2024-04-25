75d3e9454d592ac1e5023161aea1b1dfd9ff306e
Ednut001 <147618496+Ednut001@users.noreply.github.com>
2024-04-25 09:45:04 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -56,9 +56,9 @@ global.ownernumber = process.env.SUDO || '923184070915' //ur owner number
 //________________________________________________________________________________________________________
 global.ownername = process.env.SUDO_NAME || 'Salman Ahmad' //ur owner name
 //________________________________________________________________________________________________________
-global.websitex = process.env.YT_LINK || "https://youtube.com/@s4salmanyt"
+global.websitex = process.env.YT_LINK || "https://chat.whatsapp.com/HfMmL74aP3lC24AcHtebWx"
 //________________________________________________________________________________________________________
-global.wagc = process.env.GL_PUSH || "https://whatsapp.com/channel/0029VaINgEZK0IBaK1nDtE3f"
+global.wagc = process.env.GL_PUSH || "https://chat.whatsapp.com/HfMmL74aP3lC24AcHtebWx"
 //________________________________________________________________________________________________________
 global.themeemoji = process.env.GL_EMOJI || '🏮'
 //________________________________________________________________________________________________________
